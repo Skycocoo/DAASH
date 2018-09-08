7f10722bfcbc55c6f3568eb369cc616a38f85086
Skycocoo <luoyx2014@163.com>
2018-09-07 23:20:41 -0400

    Update phq in index route

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const sassMiddleware = require('node-sass-middleware');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const fetch = require('node-fetch');
-
-const phq = require('predicthq');
-let client = new phq.Client({access_token: "BsTZYhJxF0jBXE9GG7a0e7zM4JyNq9", fetch: fetch});
-
 const router = require('./route/index');
 const app = express();
 // view engine setup
@@ -39,15 +35,5 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-const latlong = '39.9526,-75.1652';
-const radius = '20km';
-const date = '2018-09-01';
-
-client.events.search({category: 'disasters, severe-weather', within: `${radius}@${latlong}`, 'active.gte': date})
-    .then((results)=>{
-        for (let event of results) {
-            console.log(event.title);
-        }
-    });
 
 module.exports = app;
--- route/index.js
+++ route/index.js
@@ -1,12 +1,29 @@
 // Created by Yuxi Luo, July 2018
 
 const express = require('express');
+const fetch = require('node-fetch');
+const phq = require('predicthq');
+let client = new phq.Client({access_token: "BsTZYhJxF0jBXE9GG7a0e7zM4JyNq9", fetch: fetch});
+
 
 module.exports = () => {
     const router = express.Router();
 
+    const latlong = '39.9526,-75.1652';
+    const radius = '20km';
+    const date = '2018-09-01';
+
+
     router.get('/', (req, res, next) => {
-        res.render('index');
+        client.events.search({category: 'disasters, severe-weather', within: `${radius}@${latlong}`, 'active.gte': date})
+            .then((results)=>{
+                let data = [];
+                for (let event of results) {
+                    // console.log(event.title);
+                    data.push(event.title);
+                }
+                res.render('index', {data: data});
+            });
     });
 
     return router;
