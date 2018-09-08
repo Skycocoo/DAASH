d9bfd1ee9b18e1b02e184942bacb457240b68112
Skycocoo <luoyx2014@163.com>
2018-09-08 16:17:09 -0400

    Writing to Firebase database

--- route/index.js
+++ route/index.js
@@ -12,20 +12,17 @@ const newsapi = new NewsAPI('bbc2f161eef842538b561fe4256186a0');
 const firebase = require('firebase');
 require("firebase/firestore");
 
-// fetch data
-function findPastData(state, county) {
-    request(`https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries?$filter=state%20eq%20%27${state}%27%20and%20declaredCountyArea%20eq%20%27${county}%20(County)%27`, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
-    });
-}
+// function findPastData(callback) {
+//     request(`https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries.json`, function (error, response, body) {
+//         if (error) callback(error, []);
+//         callback(undefined, JSON.parse(body));
+//     });
+// }
 
-function findPastDataByState(state) {
+function findPastDataByState(state, callback) {
     request(`https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries?$filter=state%20eq%20%27${state}%27`, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+        if (error) callback(error, []);
+        callback(undefined, JSON.parse(body));
     });
 }
 
@@ -101,12 +98,14 @@ function initFirebase () {
     });
 
     // Initialize Cloud Firestore through Firebase
-    var db = firebase.firestore();
+    let db = firebase.firestore();
 
     // Disable deprecated features
     db.settings({
         timestampsInSnapshots: true
     });
+
+    return db;
 
     // db.collection("users").add({
     //     first: "Ada",
@@ -123,14 +122,104 @@ function initFirebase () {
     //         console.log(`${doc.id} => ${doc.data()}`);
     //     });
     // });
+    // var messageRef = db.collection('rooms').doc('roomA')
+    //             .collection('messages').doc('message1');
 }
 
+function insertPastFirebase() {
+    let db = initFirebase();
 
+    // https://gist.github.com/dsoverby1986/df89c1ed31f3b74b1b85c73bfae1d095
+    let states = [
+        { name: 'Alabama', abbrev: 'AL' },
+        { name: 'Alaska', abbrev: 'AK' },
+        { name: 'Arizona', abbrev: 'AZ' },
+        { name: 'Arkansas', abbrev: 'AR' },
+        { name: 'California', abbrev: 'CA' },
+        { name: 'Colorado', abbrev: 'CO' },
+        { name: 'Connecticut', abbrev: 'CT' },
+        { name: 'Delaware', abbrev: 'DE' },
+        { name: 'Florida', abbrev: 'FL' },
+        { name: 'Georgia', abbrev: 'GA' },
+        { name: 'Hawaii', abbrev: 'HI' },
+        { name: 'Idaho', abbrev: 'ID' },
+        { name: 'Illinois', abbrev: 'IL' },
+        { name: 'Indiana', abbrev: 'IN' },
+        { name: 'Iowa', abbrev: 'IA' },
+        { name: 'Kansas', abbrev: 'KS' },
+        { name: 'Kentucky', abbrev: 'KY' },
+        { name: 'Louisiana', abbrev: 'LA' },
+        { name: 'Maine', abbrev: 'ME' },
+        { name: 'Maryland', abbrev: 'MD' },
+        { name: 'Massachusetts', abbrev: 'MA' },
+        { name: 'Michigan', abbrev: 'MI' },
+        { name: 'Minnesota', abbrev: 'MN' },
+        { name: 'Mississippi', abbrev: 'MS' },
+        { name: 'Missouri', abbrev: 'MO' },
+        { name: 'Montana', abbrev: 'MT' },
+        { name: 'Nebraska', abbrev: 'NE' },
+        { name: 'Nevada', abbrev: 'NV' },
+        { name: 'New Hampshire', abbrev: 'NH' },
+        { name: 'New Jersey', abbrev: 'NJ' },
+        { name: 'New Mexico', abbrev: 'NM' },
+        { name: 'New York', abbrev: 'NY' },
+        { name: 'North Carolina', abbrev: 'NC' },
+        { name: 'North Dakota', abbrev: 'ND' },
+        { name: 'Ohio', abbrev: 'OH' },
+        { name: 'Oklahoma', abbrev: 'OK' },
+        { name: 'Oregon', abbrev: 'OR' },
+        { name: 'Pennsylvania', abbrev: 'PA' },
+        { name: 'Rhode Island', abbrev: 'RI' },
+        { name: 'South Carolina', abbrev: 'SC' },
+        { name: 'South Dakota', abbrev: 'SD' },
+        { name: 'Tennessee', abbrev: 'TN' },
+        { name: 'Texas', abbrev: 'TX' },
+        { name: 'Utah', abbrev: 'UT' },
+        { name: 'Vermont', abbrev: 'VT' },
+        { name: 'Virginia', abbrev: 'VA' },
+        { name: 'Washington', abbrev: 'WA' },
+        { name: 'West Virginia', abbrev: 'WV' },
+        { name: 'Wisconsin', abbrev: 'WI' },
+        { name: 'Wyoming', abbrev: 'WY' }
+    ];
+    const initial = states.map(i => i.abbrev);
+
+    for (let i = 0; i < 1; i++) {
+        db.collection('states').add({
+            name: initial[i],
+        }).then(doc => {
+            console.log("Stroed to: ", doc.id);
+            findPastDataByState(initial[i], (err, body) => {
+                if (err) throw err;
+                let result = 0;
+
+                for (let i = 0; i < body.DisasterDeclarationsSummaries.length; i++) {
+                    if (body.DisasterDeclarationsSummaries[i].declaredCountyArea.length == 0) continue;
+                    let name = body.DisasterDeclarationsSummaries[i].declaredCountyArea.split(" ");
+                    name.splice(-1, 1);
+                    name.join(" ");
+                    result += body.DisasterDeclarationsSummaries[i].disasterNumber;
+                    doc.collection('counties').add({
+                        name: name[0],
+                        num: body.DisasterDeclarationsSummaries[i].disasterNumber,
+                        type: body.DisasterDeclarationsSummaries[i].incidentType,
+                    });
+                }
+                console.log(result);
+                let docRef = db.collection('states').doc(doc.id);
+                // wait for ~ 1min for this update
+                docRef.set({ sum: result }, { merge: true })
+                    .then(() => {console.log("Success");})
+                    .catch(err => {console.log("Error: ", err);});;
+            });
+        }).catch(err => {console.log(err);});
+    }
+}
 
 module.exports = () => {
     const router = express.Router();
 
-    initFirebase();
+    insertPastFirebase();
 
     router.get('/', (req, res, next) => {
         res.render('index');
