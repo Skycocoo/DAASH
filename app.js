df0cfd67c3c4007be791eea8cc787450de9f368d
benredmond <bjr0206@gmail.com>
2018-09-07 22:51:17 -0400

    added predicthq basic stuff

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const router = require('./route/index')
-
+const fetch = require('node-fetch');
+const phq = require('predicthq');
+let client = new phq.Client({access_token: "BsTZYhJxF0jBXE9GG7a0e7zM4JyNq9", fetch: fetch});
+const router = require('./route/index');
 const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'public/view'));
@@ -35,5 +36,16 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
+const latlong = '39.9526,-75.1652';
+const radius = '20km';
+const date = '2018-09-01';
+
+client.events.search({category: 'disasters, severe-weather', within: `${radius}@${latlong}`, 'active.gte': date})
+    .then((results)=>{
+        for (let event of results) {
+            console.log(event.title);
+        }
+    });
 
 module.exports = app;
