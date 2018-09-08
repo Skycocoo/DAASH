98c319bc64c1331661861023e915ed2d3dff8700
Skycocoo <luoyx2014@163.com>
2018-09-08 10:49:47 -0400

    Add Cloud Firestore

--- route/index.js
+++ route/index.js
@@ -2,14 +2,17 @@
 
 const express = require('express');
 const request = require('request');
-const fetch = require('node-fetch');
 
+const fetch = require('node-fetch');
 const phq = require('predicthq');
 let client = new phq.Client({access_token: "BsTZYhJxF0jBXE9GG7a0e7zM4JyNq9", fetch: fetch});
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('bbc2f161eef842538b561fe4256186a0');
 
+const firebase = require('firebase');
+require("firebase/firestore");
 
+// fetch data
 function findPastData(state, county) {
     request(`https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries?$filter=state%20eq%20%27${state}%27%20and%20declaredCountyArea%20eq%20%27${county}%20(County)%27`, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
@@ -26,7 +29,7 @@ function findPastDataByState(state) {
     });
 }
 
-// findPastData('NE', 'Pawnee');
+// findPastDataByState('NE');
 
 function findRecentData(radius, latlong, endDate, startDate) {
     client.events.search({category: 'disasters, severe-weather', within: `${radius}km@${latlong}`,
@@ -67,28 +70,70 @@ function findNews(city) { // still kinda messed up, not sure why :((((((((
     });
 }
 
-findNews("Seattle");
+// findNews("Seattle");
+
+function findDisaster(latlong, radius, date) {
+    client.events.search({category: 'disasters, severe-weather', within: `${radius}@${latlong}`, 'active.gte': date})
+        .then((results)=>{
+            let data = [];
+            for (let event of results) {
+                // console.log(event.title);
+                data.push(event.title);
+            }
+            console.log(data)
+            // res.render('index', {data: data});
+        });
+}
+
+// findDisaster('39.9526,-75.1652', '20km', '2018-09-01');
+
+
+// firebase
+
+function initFirebase () {
+    firebase.initializeApp({
+        apiKey: "AIzaSyBUYLldiIyrOQUwT8AYplBt35Utt7-QMLg",
+        authDomain: "pennapps-resiliency.firebaseapp.com",
+        databaseURL: "https://pennapps-resiliency.firebaseio.com",
+        projectId: "pennapps-resiliency",
+        storageBucket: "pennapps-resiliency.appspot.com",
+        messagingSenderId: "527178300982"
+    });
+
+    // Initialize Cloud Firestore through Firebase
+    var db = firebase.firestore();
+
+    // Disable deprecated features
+    db.settings({
+        timestampsInSnapshots: true
+    });
+
+    // db.collection("users").add({
+    //     first: "Ada",
+    //     last: "Lovelace",
+    //     born: 1815
+    // }).then(function(docRef) {
+    //     console.log("Document written with ID: ", docRef.id);
+    // }).catch(function(error) {
+    //     console.error("Error adding document: ", error);
+    // });
+    //
+    // db.collection("users").get().then((querySnapshot) => {
+    //     querySnapshot.forEach((doc) => {
+    //         console.log(`${doc.id} => ${doc.data()}`);
+    //     });
+    // });
+}
 
 
 
 module.exports = () => {
     const router = express.Router();
 
-    const latlong = '39.9526,-75.1652';
-    const radius = '20km';
-    const date = '2018-09-01';
-
+    initFirebase();
 
     router.get('/', (req, res, next) => {
-        client.events.search({category: 'disasters, severe-weather', within: `${radius}@${latlong}`, 'active.gte': date})
-            .then((results)=>{
-                let data = [];
-                for (let event of results) {
-                    // console.log(event.title);
-                    data.push(event.title);
-                }
-                res.render('index', {data: data});
-            });
+        res.render('index');
     });
 
     return router;
